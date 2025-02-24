9a340172f5b1e59d3fcefa0c270ddeb2839825e1
MukitHossen7 <hossenmukit7@gmail.com>
2025-02-24 10:47:09 +0600

    duplicate greeting in RepitComponent for enhanced visibility

--- how-to-install-react-app/src/components/RepitComponents/RepitComponent.jsx
+++ how-to-install-react-app/src/components/RepitComponents/RepitComponent.jsx
@@ -1,10 +1,16 @@
 const RepitComponent = () => {
   return (
-    <div>
+    <>
       <h2 className="text-red-500 font-bold text-center mt-10 text-3xl">
         Hello Mukit
       </h2>
-    </div>
+      <h2 className="text-red-500 font-bold text-center mt-10 text-3xl">
+        Hello Mukit
+      </h2>
+      <h2 className="text-red-500 font-bold text-center mt-10 text-3xl">
+        Hello Mukit
+      </h2>
+    </>
   );
 };
 
