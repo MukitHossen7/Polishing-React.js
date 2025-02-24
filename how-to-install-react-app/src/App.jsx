30418ba3d8546110eb60c438604643ff6e49fdbc
MukitHossen7 <hossenmukit7@gmail.com>
2025-02-24 10:11:23 +0600

    add additional RepitComponent instances to App

--- how-to-install-react-app/src/App.jsx
+++ how-to-install-react-app/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
       <RepitComponent></RepitComponent>
       <RepitComponent></RepitComponent>
       <RepitComponent></RepitComponent>
+      <RepitComponent></RepitComponent>
+      <RepitComponent></RepitComponent>
     </>
   );
 }
