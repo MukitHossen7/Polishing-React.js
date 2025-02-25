55318faec2f467ff5c9069990255d45481afb5e5
MukitHossen7 <hossenmukit7@gmail.com>
2025-02-25 16:29:14 +0600

    add ProfileCard component for user profile display

--- how-to-install-react-app/src/App.jsx
+++ how-to-install-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import ProfileCard from "./components/ProfileCard/ProfileCard";
 import RepitComponent from "./components/RepitComponents/RepitComponent";
 
 function App() {
@@ -5,6 +6,17 @@ function App() {
   return (
     <>
       <RepitComponent name={name}></RepitComponent>
+      <ProfileCard
+        name="Alice"
+        age={30}
+        greeting={
+          <div>
+            <strong>Hi Alice, have a wonderful day!</strong>
+          </div>
+        }
+      >
+        <button>Contact</button>
+      </ProfileCard>
     </>
   );
 }
---
+++ how-to-install-react-app/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+const ProfileCard = ({ name, age, greeting }) => {
+  return (
+    <div className="text-center border-2 border-gray-700 p-5 m-5 rounded-2xl">
+      <h1>Hello, {name}!</h1>
+      <h2>You are {age} years old.</h2>
+      {greeting}
+    </div>
+  );
+};
+
+export default ProfileCard;
