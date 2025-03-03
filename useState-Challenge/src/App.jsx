fa930cc615eb76745bfcfb3a42ab9a933b29d0a0
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-03 09:46:53 +0600

    project setup is done

---
+++ useState-Challenge/src/App.jsx
@@ -0,0 +1,11 @@
+import Challenge from "./components/Challenge";
+
+function App() {
+  return (
+    <>
+      <Challenge></Challenge>
+    </>
+  );
+}
+
+export default App;
---
+++ useState-Challenge/src/components/Challenge.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Challenge = () => {
+  return (
+    <div className="max-w-96 mx-auto bg-white shadow-md rounded-md">
+      <h2>Hello World</h2>
+    </div>
+  );
+};
+
+export default Challenge;
