c382296cd241234db0b8e483ea0713f162ba3c15
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-04 22:58:31 +0600

    feat: fetch and display data in NotFetchData component

--- pokeon-website/src/components/NotFetchData.jsx
+++ pokeon-website/src/components/NotFetchData.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const NotFetchData = () => {
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setApiData(data))
+      .catch((error) => console.log(error));
+  }, []);
+  // if (!apiData) return <p>Loading...</p>;
   return (
     <div>
-      <h2 className="">Fetch Data Not Right Away</h2>
+      <h2 className="text-2xl font-bold text-center mt-20">
+        Fetch Data Not Right Away
+      </h2>
+      {apiData?.map((data) => (
+        <div key={data.id}>
+          <h3>{data.title}</h3>
+        </div>
+      ))}
     </div>
   );
 };
