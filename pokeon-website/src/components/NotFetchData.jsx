159e0210a9e778d9eac0976ad8324e17e2e3add4
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-06 12:59:01 +0600

    feat: update NotFetchData component to fetch Pokémon data and handle errors

--- pokeon-website/src/components/NotFetchData.jsx
+++ pokeon-website/src/components/NotFetchData.jsx
@@ -2,28 +2,40 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const NotFetchData = () => {
-  const [apiData, setApiData] = useState(null);
+  const [apiData, setApiData] = useState([]);
 
   const fetchData = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const data = await res.json();
-    setApiData(data);
+    try {
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=24");
+      const data = await res.json();
+      const pokemonData = data.results.map(async (currentData) => {
+        const res = await fetch(currentData.url);
+        const pokemon = await res.json();
+        return pokemon;
+      });
+      console.log(pokemonData);
+      const pokemon = await Promise.all(pokemonData);
+      setApiData(pokemon);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
-  if (!apiData) return <p>Loading...</p>;
+  console.log(apiData);
+  // if (!apiData) return <p>Loading...</p>;
   return (
     <div>
       <h2 className="text-2xl font-bold text-center mt-20">
         Fetch Data Not Right Away
       </h2>
-      {apiData?.map((data) => (
+      {/* {apiData?.map((data) => (
         <div key={data.id}>
           <h3>{data.title}</h3>
         </div>
-      ))}
+      ))} */}
     </div>
   );
 };
